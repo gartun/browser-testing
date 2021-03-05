36620ff07b0f5f11a650bd687ea5afd78fc5d92a
artgökhan <62377029+gartun@users.noreply.github.com>
2021-03-05 20:35:59 +0300

    testing commit

---
+++ forms/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("button"),
+      required = document.querySelectorAll("[required]");
+
+// iterate over all required fields
+// and start the validating process
+required.forEach(field => {
+  
+});
