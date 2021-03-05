a6ef9069b3da5f514e40e146db67fb4fb1330e14
artgökhan <62377029+gartun@users.noreply.github.com>
2021-03-05 22:13:38 +0300

    my validation commit

--- forms/script.js
+++ forms/script.js
@@ -1,8 +1,40 @@
 const btn = document.querySelector("button"),
-      required = document.querySelectorAll("[required]");
+  required = document.querySelectorAll("[required]");
 
-// iterate over all required fields
-// and start the validating process
-required.forEach(field => {
-  
-});
+// Blur validation
+/*required.forEach((field) => {
+  field.addEventListener("blur", function () {
+    if (this.value.trim() === "") {
+      this.classList.add("empty");
+    }
+  });
+});*/
+
+// Button click validation
+
+btn.addEventListener("click", checkIfRequiredEmpty);
+
+function checkIfRequiredEmpty(e) {
+  e.preventDefault();
+
+  let isAllFilled = false;
+
+  for (let i = 0; i < required.length; i++) {
+    const inp = required[i];
+
+    if (inp.value.trim() === "") {
+      isAllFilled = false;
+      inp.classList.add("empty");
+      inp.focus();
+      break;
+    }
+    isAllFilled = true;
+    inp.classList.remove("empty");
+  }
+
+  if (isAllFilled) validateInput();
+}
+
+function validateInput() {
+  alert("Im here");
+}
